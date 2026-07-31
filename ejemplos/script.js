413d98d5f6995d66dbfd60ebe55e7d4b049557b2
Cruzwaldo <damiancruzwaldo@outlook.es>
2026-07-30 21:13:27 -0500

    variables

---
+++ ejemplos/script.js
@@ -0,0 +1,66 @@
+// let salario = prompt('cuanto ganas en dolares');
+// let salariocolombiano = salario *3200;
+// console.log(`tusalario en pesos colombianos es de: ${salariocolombiano}`);
+
+// let peso = prompt('cuanto pesas');
+// let pesoenlaluna = peso * 1.5;
+// console.log(`tupeso es de: ${pesoenlaluna}`);
+
+// escribir el peso de una persona
+
+
+// let nombre = "pedro" 
+// let numero = 'casa'
+
+// console.log( typeof "65");
+
+
+// cuantos años tiene en humano
+
+
+// let edadhumano = Number( prompt('cuantos años tienes'))
+// let edadperro = edadhumano * 4
+// console.log(edadperro)
+
+
+// pedir datos de un triangulo y decir el area;
+
+// let altura = Number( prompt ('digite la altura'));
+// let base = Number( prompt ('digite la base'));
+// let area = altura * base / 2
+// console.log(`area es de :${area}`);
+
+
+// pedir al usuario una cantidad de dias, 
+// y le decimos cuantos son esos en minutos14
+
+// let dias = Number(prompt('dias'));
+// let horas = dias * 24;
+
+// let minutos = horas * 60;
+// console.log(`cuantos horas ${horas}`);
+// console.log(`cuantos minutos ${minutos}`)
+
+
+
+//calcular la velocidad de un carro?
+// cuantos kilometros recorrio?
+// en cuanto tiempo?
+
+
+
+let kilometros = Number( prompt ('kilometros'));
+let tiempo = Number( prompt ('tiempo'));
+let velocidad = kilometros / tiempo
+console.log(`velocidad ${velocidad}`)
+
+
+
+
+
+
+
+
+//pedir una temperatura en C y convertirl EN f;
+
+//PEDIR LA DISTACIA EN KILOMETROS y convertirla en metros
