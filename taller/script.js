a1319afaba0e71e1309134b14ff7d16e9f27f9e8
Cruzwaldo <damiancruzwaldo@outlook.es>
2026-08-10 20:40:56 -0500

    tracer punto del taller

--- taller/script.js
+++ taller/script.js
@@ -6,5 +6,17 @@
 // console.log( 'ultimo color ' + colores [colores. length - 1])
 
 
-let numeros =[1,2,3,4,5,6,7,8,9,10]
-console.log('la cantidad de elementos es de ' + numeros.length)
+// let numeros =[1,2,3,4,5,6,7,8,9,10];
+// console.log('la cantidad de elementos es de ' + numeros.length);
+
+let numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for (let i = 1; i < numeros.length; i++) {
+    // console.log('jgyudnj')
+
+    if ( numeros[i] % 2 ===0 ){
+console.log('numeros impares')
+console.log (numeros[i])
+    }
+    
+}
