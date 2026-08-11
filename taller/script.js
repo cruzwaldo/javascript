3ef2f7832cf7d0e1358487afbf8299a5ce355b65
Cruzwaldo <damiancruzwaldo@outlook.es>
2026-08-10 20:22:32 -0500

    segundo punto del taller

--- taller/script.js
+++ taller/script.js
@@ -1,6 +1,10 @@
 
-let colores = ['azul', 'verde', 'rojo', 'morado', 'amarillo'];
+// let colores = ['azul', 'verde', 'rojo', 'morado', 'amarillo'];
 
 
-console.log('primer color ' +  colores[0]);
-console.log( 'ultimo color ' + colores [colores. length - 1])
+// console.log('primer color ' +  colores[0]);
+// console.log( 'ultimo color ' + colores [colores. length - 1])
+
+
+let numeros =[1,2,3,4,5,6,7,8,9,10]
+console.log('la cantidad de elementos es de ' + numeros.length)
